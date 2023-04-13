367d7d4f91306f08a63e4628673642cf31d65646
Angela Caldas <aes.caldas@gmail.com>
2023-04-13 17:44:43 -0300

    chore: fix some random colors with less than 6 chars

--- src/utils/randomColor.ts
+++ src/utils/randomColor.ts
@@ -1,5 +1,6 @@
 // export const randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
 export const generateRandomColor = () => {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const color = Math.floor(Math.random() * 16777215).toString(16);
+  return color.length <= 5 ? `#0${color}` : `#${color}`;
 };
