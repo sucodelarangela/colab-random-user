6d1fc8b0f9252984ec5cc4cbe3a041ceb094ad46
Angela Caldas <aes.caldas@gmail.com>
2023-04-13 10:53:11 -0300

    feat: create api logics for CRUD operations

---
+++ src/api/api.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosResponse } from 'axios';
+import { IUser } from 'interfaces/IUser';
+
+export const api = axios.create({
+  baseURL: 'https://randomuser.me/api/'
+});
+
+// Destruturando o axios e recebendo o objeto `data` de dentro dele
+const responseBody = (response: AxiosResponse) => response.data;
+
+// Objeto `request` para lidar com o cRUD e retornando o response.data acima.
+// Se houvessem outras requests (post, put, delete, etc), elas também estariam dentro desse objeto.
+const requests = {
+  get: (url: string) => api.get(url).then(responseBody)
+};
+
+// Objeto `Users` que usa o objeto `requests`para executar as operações de CRUD.
+// Se houvessem outras operações (post, put, delete, etc), elas também estariam dentro desse objeto.
+export const Users = {
+  getUsers: (page: number): Promise<IUser[]> => requests.get(`?page=${page}&results=10&exc=registered,phone,cell,nat`)
+};
